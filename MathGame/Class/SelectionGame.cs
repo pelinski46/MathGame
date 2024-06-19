46b60bd3a956dbeec066fd300538e6ec8998ecc9
Pablo <pelinski46@gmail.com>
2024-06-19 12:22:11 -0300

    Add project files.

---
+++ MathGame/Class/SelectionGame.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathGame.Class
+{
+    public class Game
+    {
+        public static void AdditionGame()
+        {
+            Console.WriteLine("Addition game selected");
+        }
+
+        public static void SubstractionGame()
+        {
+            Console.WriteLine("Substraction game selected");
+        }
+
+        public static void MultiplicationGame()
+        {
+            Console.WriteLine("Multiplication game selected");
+        }
+
+        public static void DivisionGame()
+        {
+            Console.WriteLine("Division game selected");
+        }
+    }
+
+}
---
+++ MathGame/Program.cs
@@ -0,0 +1,43 @@
+﻿using MathGame.Class;
+
+Console.WriteLine("Please type your name");
+var name = Console.ReadLine();
+var date  = DateTime.Now;
+
+Console.WriteLine("----------------------------------------------------------");
+Console.WriteLine($"Hello {name}. It's {date}. This is your math's game\n");
+Console.WriteLine($@"What game would you like to play today? choose from the options below:
+A - Addition 
+S - Subtraction 
+M - Multiplication
+D - Divison
+Q - Quit the program");
+Console.WriteLine("----------------------------------------------------------");
+
+var gameSelected = Console.ReadLine()!.Trim().ToUpper();
+
+if (gameSelected == "A")
+{
+    Game.AdditionGame();
+}
+else if (gameSelected == "S")
+{
+    Game.SubstractionGame();
+}
+else if (gameSelected == "M")
+{
+    Game.MultiplicationGame();
+}
+else if (gameSelected == "D")
+{
+    Game.DivisionGame();
+}
+else if (gameSelected == "Q")
+{
+    Console.WriteLine("Goodbye");
+    Environment.Exit(1);
+}
+else
+{
+    Console.WriteLine("Invalid input");
+}
